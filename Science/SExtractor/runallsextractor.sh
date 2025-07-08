ad04d788fcacbc7391273e7f7b932aaf056f3a41
Will Waldron <Will.Waldron@uah.edu>
2025-07-08 12:32:21 -0500

    Updated the SExtractor bash run files

--- Science/SExtractor/runallsextractor.sh
+++ Science/SExtractor/runallsextractor.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 # Runs all the SExtractor Files
 
+# Export Needed Directories
+export CONF_DIR="/home/wwaldron/Research/Galaxies/ESO/ESO-137-001/Science/SExtractor/PhotometryRunFiles"
+export IMG_DIR="/home/wwaldron/Research/Galaxies/ESO/ESO-137-001/Images/ProcessedImages/HST/Drizzled"
+
 ./sextractorcommandsF275W.sh
 ./sextractorcommandsF475W.sh
 ./sextractorcommandsF814W.sh
 ./sextractorcommandsF160W.sh
-./sextractorcommandsOrig.sh
--- Science/SExtractor/sextractorcommandsF160W.sh
+++ Science/SExtractor/sextractorcommandsF160W.sh
@@ -2,20 +2,18 @@
 #Commands run to run SExtractor for the 4 images using F160W as the detection
 #image.
 
-# SExtractor needs to be run in the directory where the measurement file is
-# sitting
-cd ../DrizzledImages/
+# Note that the calling file is runallsextractor.sh
+# This file defines $CONF_DIR and $IMG_DIR, which are used in the commands below.
 
-# Run for F160W
-sextractor F160W_drz.fits[1],F160W_drz.fits[1] -c ../SExtractor/PhotometryRunFiles/F160W/ESO_dual_F160WxF160W.conf
+# SExtractor needs to be run in the directory where the measurement/analysis
+# image is located
+cd $IMG_DIR
 
-# Move to directory where the rest of the files are sitting
-cd ../CleanedImages/
-
-# Run for remaining images
-sextractor ../DrizzledImages/F160W_drz.fits[1],F275W_drc_cleaned.fits[1] -c ../SExtractor/PhotometryRunFiles/F160W/ESO_dual_F160WxF275W.conf
-sextractor ../DrizzledImages/F160W_drz.fits[1],F475W_drc_chip_cleaned.fits[1] -c ../SExtractor/PhotometryRunFiles/F160W/ESO_dual_F160WxF475W.conf
-sextractor ../DrizzledImages/F160W_drz.fits[1],F814W_drc_chip_cleaned.fits[1] -c ../SExtractor/PhotometryRunFiles/F160W/ESO_dual_F160WxF814W.conf
+# Run for All Images
+sextractor ESO137-001-F160W_drz.fits[1],ESO137-001-F160W_drz.fits[1] -c $CONF_DIR/F160W/ESO_dual_F160WxF160W.conf
+sextractor ESO137-001-F160W_drz.fits[1],ESO137-001-F275W_drc.fits[1] -c $CONF_DIR/F160W/ESO_dual_F160WxF275W.conf
+sextractor ESO137-001-F160W_drz.fits[1],ESO137-001-F475W_drc.fits[1] -c $CONF_DIR/F160W/ESO_dual_F160WxF475W.conf
+sextractor ESO137-001-F160W_drz.fits[1],ESO137-001-F814W_drc.fits[1] -c $CONF_DIR/F160W/ESO_dual_F160WxF814W.conf
 
 # Return to original directory
-cd ../Programming/
+cd -
